9856a6cf2138e5d900fde3653015fbf038206286
Andreas Finger <andreas.finger@xing.com>
2014-07-31 23:34:56 +0200

    Add validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :project
+
+  validates :name,   presence: true
+  validates :status, inclusion: { in: %w(todo doing done), message: "%{value} is not a valid status" }
 end
