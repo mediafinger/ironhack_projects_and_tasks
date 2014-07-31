fc290ddfdc3da61180591b689852803141054859
Andreas Finger <andreas.finger@xing.com>
2014-08-01 01:14:57 +0200

    Display Project Status

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,16 @@ class Project < ActiveRecord::Base
   has_many :tasks
 
   validates :name,   presence: true
+
+  def status
+    statuses = tasks.pluck(:status).uniq
+
+    if statuses.include? "todo"
+      "todo"
+    elsif statuses.include? "doing"
+      "doing"
+    elsif statuses.include? "done"
+      "done"
+    end
+  end
 end
